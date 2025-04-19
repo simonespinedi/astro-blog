5113d4197cf1612a52174630fb6a9b828952e055
Simone Spinedi <simonespinedipro@gmail.com>
2025-04-19 12:28:54 +0200

    fixed posititioning, buttons and responsiveness

--- src/scripts/menu-toggle.js
+++ src/scripts/menu-toggle.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('menu-toggle');
-    const menu = document.getElementById('right-buttons');
-  
-    toggleButton.addEventListener('click', () => {
-      menu.classList.toggle('show');
+  const toggleButton = document.getElementById('menu-toggle');
+  const menu = document.getElementById('right-buttons');
+
+  toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
+  // Chiudi il menu se si clicca su uno dei link
+  document.querySelectorAll('#right-buttons a').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('show');
     });
   });
+});
